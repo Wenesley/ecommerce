5e60745a66fdece89cc3204d97cc4925aae1d74f
Wenesley <wenesleysantos@gmail.com>
2020-04-05 17:11:38 -0300

    criamos a classe Page

--- index.php
+++ index.php
@@ -2,16 +2,24 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Wenesley\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
 
+	$page = new Page();
+	$page->setTpl("index");
+
+	/*testes desta classe
 	$sql = new Wenesley\DB\Sql();
 	$results = $sql->select("SELECT * FROM tb_users");
 
 	echo json_encode($results);
+	*/
     
 	//echo "OK";
 
